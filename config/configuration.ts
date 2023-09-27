2e3946fe73351fa3f57a19af5fb5ff96ef8fd2d3
vodis <i40217891@gmail.com>
2023-09-27 23:03:23 +0100

    feat/CSS-7 - knex settings
    - use secrets

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,23 +1,3 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
-  database: {
-    client: process.env.DB_CLIENT,
-    connection: {
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10) || 5432,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    pool: {
-      min: 2,
-      max: 20,
-    },
-    migrations: {
-      tableName: process.env.DB_MIGRATIONS_TN,
-    },
-    seeds: {
-      directory: process.env.DB_SEED_PATH,
-    },
-  },
 });

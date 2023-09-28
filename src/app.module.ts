4261f5b53ef37c7c673ab0fa8ef8ee422947615f
vodis <i40217891@gmail.com>
2023-09-28 09:19:30 +0100

    feat/CSS-7 - knex settings
    - use nest-knexjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from '../config/configuration';
+import { KnexModule } from 'nest-knexjs';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
+    }),
+    KnexModule.forRoot({
+      config: configuration().database,
     }),
   ],
   controllers: [AppController],

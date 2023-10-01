aff49f996f7b3c4829452c50349a124c26f3ff40
vodis <i40217891@gmail.com>
2023-10-01 16:46:21 +0100

    feat/CSS-9 - docker compose settings
    - fix knex migrate up

--- knexfile.js
+++ knexfile.js
@@ -8,5 +8,15 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
     },
+    pool: {
+      min: 2,
+      max: 20,
+    },
+    migrations: {
+      tableName: process.env.DB_MIGRATIONS_TN,
+    },
+    seeds: {
+      directory: process.env.DB_SEED_PATH,
+    },
   },
 };

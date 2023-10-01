2e3aa63cde2d5aceaa4e9fca6248acaaa4da29a0
vodis <i40217891@gmail.com>
2023-10-01 20:34:55 +0100

    feat/CSS-9 - docker compose settings
    - comment deploy flow

--- knexfile.js
+++ knexfile.js
@@ -8,16 +8,9 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
     },
-    acquireConnectionTimeout: 1000000,
     pool: {
       min: 2,
       max: 20,
-      acquireTimeoutMillis: 300000,
-      createTimeoutMillis: 300000,
-      destroyTimeoutMillis: 300000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 2000,
     },
     migrations: {
       tableName: process.env.DB_MIGRATIONS_TN,

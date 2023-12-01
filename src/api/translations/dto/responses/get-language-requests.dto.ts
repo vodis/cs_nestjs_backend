b223b0740c75940b273d56421aa1aa0453964379
vodis <i40217891@gmail.com>
2023-12-01 17:46:36 +0000

    feat/CSS-14 - Update translations requests
    - delete outdated bd directory
    - update GetLanguageRequestsDto
    - add i18n api endpoint getLanguages
    - add cors
    - add swagger config

---
+++ src/api/translations/dto/responses/get-language-requests.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
+import { IsString } from 'class-validator';
+
+class LanguageDetails {
+    @ApiProperty()
+    @Expose()
+    @IsString()
+    language: string;
+
+    @ApiProperty()
+    @Expose()
+    @IsString()
+    title: string;
+}
+
+class LanguagesIndexer {
+    [indexer: string]: LanguageDetails;
+}
+
+class translationsIndexer {
+    [indexer: string]: LanguageDetails;
+}
+
+export class GetLanguageRequestsDto {
+    @ApiProperty()
+    @Expose()
+    @Type(() => LanguageDetails)
+    languages: LanguagesIndexer;
+
+    @ApiProperty()
+    @Expose()
+    @Type()
+    translations: translationsIndexer;
+}
